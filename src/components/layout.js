72783bf97562d15e60244cf21ab31d57ef3d0305
Lukas Zanner <git@lukaszanner.de>
2020-07-15 13:32:08 +0200

    Remove bootstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Head from "./head";
-import Header from "./header";
 import Footer from "./footer";
 
 import styles from "./layout.module.css";
@@ -8,8 +6,6 @@ import styles from "./layout.module.css";
 const Layout = ({ children }) => {
     return (
         <>
-            <Head />
-            <Header />
             <div className={styles.layout}>
                 <div className={styles.container}>
                     {children}
