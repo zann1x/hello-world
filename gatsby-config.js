39adbd26b5a4370ae0064e21087e3144d66c0705
Lukas Zanner <git@lukaszanner.de>
2020-07-20 18:33:54 +0200

    Reduce max width of project images

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 560
+              maxWidth: 400
             }
           },
           {
