3cc7f25392744bf094a3353a6bbc9cd844936f61
Lukas Zanner <git@lukaszanner.de>
2020-07-25 18:52:25 +0200

    Rework index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 import SEO from "../components/seo";
 
 import styles from "./index.module.css";
-import ExternalLink from "../components/external_link";
+import Footer from "../components/footer";
 
 const IndexPage = () => (
     <>
@@ -11,23 +11,15 @@ const IndexPage = () => (
 
         <div className={styles.content}>
             <h1 className={styles.heading}>lukas zanner // software developer</h1>
-            <div className={styles.blocks}>
-                <div className={styles.link_block}>
-                    <p>links</p>
-                    <p>&gt; <Link to="/about/" title="About me">about</Link></p>
-                    <p>&gt; <Link to="/projects/" title="Projects">projects</Link></p>
-                    <p>&gt; <Link to="/work/" title="Work">work</Link></p>
-                    <p>&gt; <Link to="/education/" title="Education">education</Link></p>
-                </div>
-                <div className={styles.contact_block}>
-                    <p>contact</p>
-                    <p>&gt; <ExternalLink to="mailto:hi@lukaszanner.de?subject=message from lukaszanner.de" title="Email">email</ExternalLink></p>
-                    <p>&gt; <ExternalLink to="https://github.com/zann1x" title="GitHub">github</ExternalLink></p>
-                    <p>&gt; <ExternalLink to="https://linkedin.com/in/lukas-zanner" title="LinkedIn">linkedin</ExternalLink></p>
-                    <p>&gt; <ExternalLink to="https://www.xing.com/profile/Lukas_Zanner" title="XING">xing</ExternalLink></p>
-                </div>
+            <div className={styles.link_block}>
+                <p>$ ls -l</p>
+                <p><Link to="/about/" title="About me">about/</Link></p>
+                <p><Link to="/projects/" title="Projects">projects/</Link></p>
+                <p><Link to="/work/" title="Work">work/</Link></p>
+                <p><Link to="/education/" title="Education">education/</Link></p>
             </div>
         </div>
+        <Footer />
     </>
 );
 
