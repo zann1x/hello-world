7a61ce29e71cf746589c8d9b98077a136b0de108
Lukas Zanner <git@lukaszanner.de>
2020-07-25 23:56:17 +0200

    Set footer icon size directly on the component

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,13 +9,16 @@ import styles from "./footer.module.css";
 const Footer = () => (
     <footer className={styles.container}>
         <ExternalLink to="mailto:hi@lukaszanner.de?subject=message from lukaszanner.de" title="Email">
-            <FontAwesomeIcon icon={faEnvelope} />
-        </ExternalLink> - <ExternalLink to="https://github.com/zann1x" title="GitHub">
-            <FontAwesomeIcon icon={faGithub} />
-        </ExternalLink> - <ExternalLink to="https://linkedin.com/in/lukas-zanner" title="LinkedIn">
-            <FontAwesomeIcon icon={faLinkedinIn} />
-        </ExternalLink> - <ExternalLink to="https://www.xing.com/profile/Lukas_Zanner" title="XING">
-            <FontAwesomeIcon icon={faXing} />
+            <FontAwesomeIcon icon={faEnvelope} size="2x" />
+        </ExternalLink>
+        <ExternalLink to="https://github.com/zann1x" title="GitHub">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+        </ExternalLink>
+        <ExternalLink to="https://linkedin.com/in/lukas-zanner" title="LinkedIn">
+            <FontAwesomeIcon icon={faLinkedinIn}  size="2x" />
+        </ExternalLink>
+        <ExternalLink to="https://www.xing.com/profile/Lukas_Zanner" title="XING">
+            <FontAwesomeIcon icon={faXing} size="2x" />
         </ExternalLink>
     </footer>
 );
