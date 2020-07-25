11f216d57c44dfe90f7c32e55321d30725faa359
Lukas Zanner <git@lukaszanner.de>
2020-07-26 00:14:56 +0200

    Remove redundant div in page template

--- src/templates/SiteTemplate.js
+++ src/templates/SiteTemplate.js
@@ -24,7 +24,9 @@ const SiteTemplate = ({ data }) => {
         <Layout>
             <SEO title={frontmatter.title} description={frontmatter.description} />
             {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
-            <header><div><Link to="/">cd ../</Link></div></header>
+            <header>
+                <Link to="/">cd ../</Link>
+            </header>
             <h1>{frontmatter.title}</h1>
             <div className={styles.content} dangerouslySetInnerHTML={{ __html: html }} />
         </Layout>
