9c9809a1f64454937e37b1b7d4215c541853cfad
Lukas Zanner <git@lukaszanner.de>
2020-10-14 20:25:07 +0200

    Simplify start page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link } from "gatsby";
+// import { Link } from "gatsby";
 import SEO from "../components/seo";
 
 import styles from "./index.module.css";
-import Footer from "../components/footer";
+import ExternalLink from "../components/external_link";
+// import Footer from "../components/footer";
 
 const IndexPage = () => (
     <>
@@ -13,12 +14,34 @@ const IndexPage = () => (
             <div className={styles.link_block}>
                 <p>lukas@void:~$ ls -l</p>
                 <p>total 4</p>
+                {/*
                 <p><Link to="/about/" title="About me">about/</Link></p>
                 <p><Link to="/projects/" title="Projects">projects/</Link></p>
                 <p><Link to="/work/" title="Work">work/</Link></p>
                 <p><Link to="/education/" title="Education">education/</Link></p>
+                */}
+                <p>
+                    <ExternalLink to="mailto:hi@lukaszanner.de?subject=message from lukaszanner.de" title="Email">
+                        mail/
+                    </ExternalLink>
+                </p>
+                <p>
+                    <ExternalLink to="https://github.com/zann1x" title="GitHub">
+                        github/
+                    </ExternalLink>
+                </p>
+                <p> <ExternalLink to="https://linkedin.com/in/lukas-zanner" title="LinkedIn">
+                        linkedin/
+                    </ExternalLink>
+                </p>
+                <p> <ExternalLink to="https://www.xing.com/profile/Lukas_Zanner" title="XING">
+                        xing/
+                    </ExternalLink>
+                </p>
             </div>
+            {/*
             <Footer />
+            */}
         </div>
     </>
 );
