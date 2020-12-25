2380673c20660c606f22bb58b3f97599f59e96f8
Lukas Zanner <git@lukaszanner.de>
2020-12-25 22:33:45 +0100

    Add PWA functionality

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const assets = [
+    "/",
+    "/index.html"
+];
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open("lukas-zanner").then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
